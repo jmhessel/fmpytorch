3bb9e062cbc0d95f91585ac37ee3f9654cb985ea
Jack Hessel <jmhessel@fb.com>
2017-08-14 23:55:42 -0700

    fixed no bounds check stuff... lets just not have cdef functions return values

--- fm_fast_inner.pyx
+++ fm_fast_inner.pyx
@@ -12,7 +12,7 @@ from cython.view cimport array as cvarray
 
 ctypedef np.float64_t REAL_t
 
-
+@cython.boundscheck(False)
 cdef void _compute_sop_sos(REAL_t[:] sop,
                            REAL_t[:] sos,
                            REAL_t[:] x,
@@ -25,14 +25,16 @@ cdef void _compute_sop_sos(REAL_t[:] sop,
             sop[f] = sop[f] + v[i,f] * x[i]
             sos[f] = sos[f] + v[i,f]*v[i,f] * x[i]*x[i]
 
-cdef REAL_t _compute_output(REAL_t[:] sop,
-                            REAL_t[:] sos,
-                            int n_factors) nogil:
-    cdef REAL_t output
+@cython.boundscheck(False)
+cdef void _compute_output(REAL_t[:] sop,
+                          REAL_t[:] sos,
+                          int n_factors,
+                          REAL_t[:] output) nogil:
     cdef int f
+    cdef int zero = 0
     for f in range(n_factors):
-        output = output + sop[f] * sop[f] - sos[f]
-    return output * .5
+        output[zero] = output[zero] + sop[f] * sop[f] - sos[f]
+    output[zero] = output[zero] * .5
 
 def fast_forward(self, x, w0, w1, v):
     self.x = x
@@ -54,8 +56,11 @@ def fast_forward(self, x, w0, w1, v):
                      self.n_feats,
                      self.n_factors)
 
-    output_factor = _compute_output(self.sum_of_products,
-                                    self.sum_of_squares,
-                                    self.n_factors)
+    output_factor = np.zeros(1)
+    _compute_output(self.sum_of_products,
+                    self.sum_of_squares,
+                    self.n_factors,
+                    output_factor)
+    output_factor = output_factor[0]
 
     return w0 + torch.dot(x,w1) + output_factor
