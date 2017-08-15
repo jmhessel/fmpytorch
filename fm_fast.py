3c00b548c67e26d7a062642a3717a1dfb6d04e4e
Jack Hessel <jmhessel@fb.com>
2017-08-15 00:07:51 -0700

    somewhat fully cythonized

--- fm_fast.py
+++ fm_fast.py
@@ -1,7 +1,7 @@
 import numpy as np
 import time
 import torch
-from fm_fast_inner import fast_forward
+from fm_fast_inner import fast_forward, fast_backward
 
 class SecondOrderFM(torch.autograd.Function):
     def forward(self, x, w0, w1, v):
@@ -9,20 +9,4 @@ class SecondOrderFM(torch.autograd.Function):
         return fast_forward(self, x, w0, w1, v)
 
     def backward(self, grad_output):
-        tmp_grad_input = torch.zeros(self.n_feats).double()
-        for i in range(self.n_feats):
-            for f in range(self.n_factors):
-                tmp_grad_input[i] += self.sum_of_products[f] * self.v[i,f]
-                tmp_grad_input[i] -= self.x[i] * self.v[i,f]**2
-
-        grad_input = torch.mul(self.w1 + tmp_grad_input, grad_output)
-        grad_w0 = torch.mul(torch.ones(1).double(), grad_output)
-        grad_w1 = torch.mul(self.x, grad_output)
-        grad_v = torch.zeros(self.n_feats, self.n_factors).double()
-        for i in range(self.n_feats):
-            for f in range(self.n_factors):
-                grad_v[i,f] = self.x[i] * self.sum_of_products[f]
-                grad_v[i,f] -= self.v[i,f] * self.x[i]**2
-
-        grad_v = torch.mul(grad_v, grad_output)
-        return grad_input, grad_w0, grad_w1, grad_v
+        return fast_backward(self, grad_output)
--- fm_fast_inner.pyx
+++ fm_fast_inner.pyx
@@ -64,3 +64,53 @@ def fast_forward(self, x, w0, w1, v):
     output_factor = output_factor[0]
 
     return w0 + torch.dot(x,w1) + output_factor
+
+@cython.boundscheck(False)
+cdef void _compute_tmp_grad_input(REAL_t[:] sop,
+                                  REAL_t[:] x,
+                                  REAL_t[:,:] v,
+                                  int n_feats,
+                                  int n_factors,
+                                  REAL_t[:] tgi) nogil:
+    cdef int f,i
+    for f in range(n_factors):
+        for i in range(n_feats):
+            tgi[i] = tgi[i] + sop[f] * v[i,f]
+            tgi[i] = tgi[i] - x[i] * v[i,f]*v[i,f]
+
+@cython.boundscheck(False)
+cdef void _compute_grad_v(REAL_t[:] sop,
+                          REAL_t[:] x,
+                          REAL_t[:,:] v,
+                          int n_feats,
+                          int n_factors,
+                          REAL_t[:,:] grad_v) nogil:
+    cdef int f,i
+    for f in range(n_factors):
+        for i in range(n_feats):
+            grad_v[i,f] = x[i] * sop[f] - v[i,f] * x[i] * x[i]
+
+
+def fast_backward(self, grad_output):
+    tmp_grad_input = torch.zeros(self.n_feats).double()
+    _compute_tmp_grad_input(self.sum_of_products,
+                            self.x.numpy(),
+                            self.v.numpy(),
+                            self.n_feats,
+                            self.n_factors,
+                            tmp_grad_input.numpy())
+
+    grad_input = torch.mul(self.w1 + tmp_grad_input, grad_output)
+    grad_w0 = torch.mul(torch.ones(1).double(), grad_output)
+    grad_w1 = torch.mul(self.x, grad_output)
+    grad_v = torch.zeros(self.n_feats, self.n_factors).double()
+
+    _compute_grad_v(self.sum_of_products,
+                    self.x.numpy(),
+                    self.v.numpy(),
+                    self.n_feats,
+                    self.n_factors,
+                    grad_v.numpy())
+
+    grad_v = torch.mul(grad_v, grad_output)
+    return grad_input, grad_w0, grad_w1, grad_v
