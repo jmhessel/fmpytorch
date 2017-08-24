2f1d61e83c390276ebb0d3bc1ce884de6fb824da
Jack Hessel <jmhessel@gmail.com>
2017-08-24 14:10:29 -0400

    trying out this whole setup file thing

---
+++ setup.py
@@ -0,0 +1,30 @@
+import numpy
+from setuptools import setup, find_packages, Extension
+
+extensions = [
+    Extension('fmpytorch.second_order.second_order_fast_inner',
+              ["fmpytorch/second_order/second_order_fast_inner.c"],
+              include_dirs=[numpy.get_include()]),
+]
+
+
+setup(name="fmpytorch",
+            version="0.1",
+            description="A fast factorization machine in pytorch",
+            author="Jack Hessel",
+            author_email="jmhessel@gmail.com",
+            license="MIT",
+            packages=find_packages(),
+            ext_modules=extensions,
+            install_requires=['numpy', 'pytorch'],
+            zip_safe=False,
+            classifiers=[
+                'Intended Audience :: Science/Research',
+                'Intended Audience :: Developers', 'License :: OSI Approved',
+                'Programming Language :: C', 'Programming Language :: Python',
+                'Topic :: Software Development',
+                'Topic :: Scientific/Engineering',
+                'Operating System :: Microsoft :: Windows',
+                'Operating System :: POSIX', 'Operating System :: Unix',
+                'Operating System :: MacOS']
+)
