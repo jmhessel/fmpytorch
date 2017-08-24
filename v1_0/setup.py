a5b2697c1abe27560a72bc8d6689b4635c8d7873
Jack Hessel <jmhessel@gmail.com>
2017-08-24 14:01:08 -0400

    removed initial code

--- v1_0/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    ext_modules = cythonize("fm_fast_inner.pyx"),
-    include_dirs=[numpy.get_include()]
-)
